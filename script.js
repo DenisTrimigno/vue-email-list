936505ab0324482a9d9d625b92b4c2fc9f62c114
MyUsernameDenisTrimignogit config --global user.email my@email.comdenis@trimigno.it <denis@trimigno.it>
2023-06-20 17:41:09 +0200

    js corretto per stampa su html

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ createApp ({
         for(let i=1; i<=10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) =>
             {
-                console.log(response.data.response)
+                this.arrayEmail.push(response.data.response);
             })
         }
     }
